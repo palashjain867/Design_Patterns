406a4c258f31417d54d9d8f94734edfd80990325
Palash Jain <palashjain867@gmail.com>
2025-04-01 05:01:27 +0000

    Strategy design pattern implementation

---
+++ StrategyDesignPattern/StrategyMain.cpp
@@ -0,0 +1,74 @@
+
+#include "iostream"
+
+using namespace std;
+
+class myStrategyPayment{
+    public:
+        virtual void execute() = 0; 
+        ~myStrategyPayment(){
+
+        }
+};
+
+class CreditCardPayment : public myStrategyPayment{
+    public:
+        void execute(){
+            cout<<"Executing Credit card payment" << "\n";
+        }
+    
+        CreditCardPayment(){
+
+        }
+        ~CreditCardPayment(){
+
+        } //what should be the definitions of these
+
+};
+
+class PaypalPayment : public myStrategyPayment{
+    public:
+        void execute(){
+            cout<<"Executing PayPal Payment " << "\n";
+        }
+
+        PaypalPayment(){
+
+        }
+        ~PaypalPayment(){
+
+        }
+};
+
+class myContext{
+    public:
+    myStrategyPayment* myStrategyPtr;
+
+    void setStrategy(myStrategyPayment* ptr){
+        myStrategyPtr = ptr;
+    }
+
+    void executeStrategy()
+    {
+        myStrategyPtr->execute();
+    }
+};
+
+int main()
+{
+
+    myContext obj;
+    myStrategyPayment* ptr = new PaypalPayment();
+    obj.setStrategy(ptr);
+    obj.executeStrategy();
+
+    delete ptr;
+
+    ptr = new CreditCardPayment();
+    obj.setStrategy(ptr);
+    obj.executeStrategy();
+
+    delete ptr;
+    
+    return 0;
+}
